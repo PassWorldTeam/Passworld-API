93ddf942ec474f92c6b239c1061ffc2a1be02a58
RemRem <remrem@github.com>
2022-11-30 17:27:42 +0100

    tw

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -59,7 +59,7 @@ class API {
     if (await checkRequiredFields(required, body)) {
       AccountsToPostgres db = AccountsToPostgres();
       db.create(body[required[0]], body[required[1]], body[required[2]],
-          body[required[3]]);
+          body[required[3]] as List<String>);
       return Response.ok('true');
     } else {
       return Response.badRequest();
